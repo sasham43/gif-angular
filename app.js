4a3c24c9410f6e523b306551f330e4449d9bfbdf
sasham43 <sashasemail@gmail.com>
2016-04-08 09:20:37 -0500

    Add carousel logic for gif search, only show one at a time.  Add custom directive for random gifs so it doesn't switch from loading animation to gif until gif is loaded

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 var app = angular.module("gifApp", []);
 
-app.controller('GifController', function($scope, $http){
+var gifLoaded = false;
+
+app.directive('imageonload', function(){
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+            element.bind('load', function() {
+                // alert('image is loaded');
+                gifLoaded = true;
+                console.log('gif loaded');
+            });
+        }
+  };
+});
+
+app.controller('GifController', function($scope, $http, $interval){
 
   $scope.searchIcon = 'search.png';
   $scope.randomIcon = 'shuffle.png';
 
-
-
+  $scope.gifIndex = 0;
+  $scope.gifList = [];
 
   $scope.imageLoading = false;
   $scope.showGif = false;
+
+  $scope.imageLoading = false;
+  $scope.showGif = true;
 
   $scope.getRandomGif = function(){
     $scope.showGif = false;
@@ -18,36 +36,60 @@ app.controller('GifController', function($scope, $http){
     $http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC').then(function(response){
       console.log(response);
       $scope.gif = response.data.data.image_url;
-      $scope.imageLoading = false;
-      $scope.showGif = true;
+
+      // not working
+      $scope.waitForLoad();
+
+
     });
   }
 
+  $scope.waitForLoad = function(){
+    var waiting = $interval(function(){
+      if(gifLoaded){
+        $scope.imageLoading = false;
+        $scope.showGif = true;
+        return;
+      }
+    }, 100);
+  }
+
   $scope.searchGifs = function(){
+    $scope.gifIndex = 0;
     console.log('search');
     $scope.showGif = false;
     $scope.imageLoading = true;
     $http.get('http://api.giphy.com/v1/gifs/search?q=' + $scope.searchText + '&api_key=dc6zaTOxFJmzC').then(function(response){
       console.log(response);
-       $scope.gifList = response.data.data;
-       $scope.imageLoading = false;
+      $scope.gifList = response.data.data;
+      $scope.imageLoading = false;
     });
+
+    var gifInterval = $interval(function(){
+      if($scope.gifIndex < $scope.gifList.length){
+        $scope.gifIndex++
+      } else {
+        $interval.cancel(gifInterval);
+      }
+
+      console.log($scope.gifIndex);
+    }, 2500);
   }
 
   $scope.searchMode = function(){
     $scope.showRandom = false;
     $scope.showSearch = true;
     $scope.changeIcons();
-    // $scope.searchStyle = {'border': 'solid 4px #646FB8', 'width': '34px', 'height': '34px'};
-    // $scope.randomStyle = {'border': 'none'};
+    $scope.searchStyle = {'background-color': '#263696'};
+    $scope.randomStyle = {'background-color': 'inherit'};
   }
 
   $scope.randomMode = function(){
     $scope.showSearch = false;
     $scope.showRandom = true;
     $scope.changeIcons();
-    // $scope.searchStyle = {'border': 'none'};
-    // $scope.randomStyle = {'border': 'solid 4px #646FB8'};
+    $scope.searchStyle = {'background-color': 'inherit'};
+    $scope.randomStyle = {'background-color': '#263696'};
   }
 
   $scope.changeIcons = function(){
